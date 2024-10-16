e9752f88932d3a44e36eadde1a64c49248835c89
KentTDang <kent_2015@outlook.com>
2024-10-15 22:50:58 -0400

    👋 Hand detector is functioning properly with the video component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,60 @@
-import {} from 'react'
+// import DetectHands from "./components/DetectHands";
+// import Media from "./components/Media";
+import { useRef, useEffect } from "react";
+import * as handPoseDetection from "@tensorflow-models/hand-pose-detection";
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const model = handPoseDetection.SupportedModels.MediaPipeHands;
+  const detectorRef = useRef<handPoseDetection.HandDetector | null>(null);
+
+  useEffect(() => {
+    const fetchMedia = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: { width: 1280, height: 720 },
+        });
+
+        videoRef.current!.srcObject = stream;
+        detectHands();
+      } catch (e) {
+        console.error("Error fetching media: ", e);
+      }
+    };
+    fetchMedia();
+  });
+
+  const detectHands = async () => {
+    console.log("Hit 1 detect hands");
+
+    try {
+      console.log("Hit inside the try");
+      // This detector detects hands
+      detectorRef.current = await handPoseDetection.createDetector(model, {
+        runtime: "mediapipe", // or 'tfjs'
+        modelType: "full",
+        solutionPath: "https://cdn.jsdelivr.net/npm/@mediapipe/hands",
+      });
+
+      console.log(detectorRef.current);
+
+      if (videoRef.current) {
+        const hands = await detectorRef.current.estimateHands(videoRef.current);
+        console.log(hands);
+      }
+    } catch (e) {
+      console.error("Error Detecting Hands: ", e);
+    }
+
+    console.log("Hit 2 detect hands");
+    // detectHands();
+  };
 
   return (
     <>
-      <h1>Hello World</h1>
+      <video ref={videoRef} autoPlay />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
